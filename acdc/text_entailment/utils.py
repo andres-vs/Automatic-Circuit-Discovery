1fe5d79f2a8f84098cb41ee5537a7df9b647319f
Andres Van Schel <r0806556@esat.kuleuven.be>
2024-04-17 23:57:35 +0200

    added corrupted data generation

--- acdc/text_entailment/utils.py
+++ acdc/text_entailment/utils.py
@@ -7,8 +7,9 @@ from transformer_lens.HookedTransformer import HookedTransformer
 
 from huggingface_hub import login
 from datasets import load_dataset
-from transformers import AutoTokenizer
 import random
+from datasets import Dataset
+from tqdm import tqdm
 
 def remove_special_tokens(example):
     example['input'] = example['input'].replace('[CLS]', '')
@@ -26,14 +27,38 @@ def get_bert_base_uncased(device):
         tl_model.set_use_hook_mlp_in(True)
     return tl_model
 
+def invert_query(query):
+    if 'not' in query:
+        return query.replace('not ', '')
+    else:
+        return query.replace(' ', ' not ', -1)
+def generate_corrupt_examples(examples):
+    inputs = []
+    labels = []
+    for example in examples:
+        input_text = example['input']
+        theory, query = input_text.split('[SEP]')
+        if example['label']:
+            if "not" in query:
+                theory = theory + ' ' + invert_query(query)
+            else:
+                theory = theory.replace(query, '')
+        else:
+            if "not" in query:
+                theory = theory.replace(invert_query(query), '')
+            else:
+                theory = theory + ' ' + query
+        inputs.append(theory + '[SEP]' + query)
+        labels.append(not example['label'])
+    return Dataset.from_dict({'input': inputs, 'label': labels})
+
 def get_all_text_entailment_things(num_examples, device, metric_name, kl_return_one_element=True):
     tl_model=get_bert_base_uncased(device)
 
     login(token="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", add_to_git_credential=True)
     dataset_name = "andres-vs/ruletaker-Att-Noneg-depth0"
     # as you do sentence classification in this task, I used a classification model instead of an autoregressive one
-    model_name = "bert-base-uncased"
-    # tokenizer = AutoTokenizer.from_pretrained(model_name)
+    # model_name = "bert-base-uncased"
 
     dataset = load_dataset(dataset_name)
     test_size = len(dataset["test"])
@@ -43,20 +68,21 @@ def get_all_text_entailment_things(num_examples, device, metric_name, kl_return_
         raise ValueError("num_examples cannot exceed half of the test split size.")
     
     examples = examples.map(remove_special_tokens)
-    # tokenized_examples = examples.map(lambda x: tokenize_function(tokenizer, x), batched=True)
-    # print(tokenized_examples)
+    corrupted_examples = generate_corrupt_examples(examples)
+    
     validation_data = examples[:num_examples]["input"]
+    validation_patch_data = corrupted_examples[:num_examples]["input"]
     validation_labels = examples[:num_examples]["label"]
     test_data = examples[num_examples:]["input"]
+    test_patch_data = corrupted_examples[num_examples:]["input"]
     test_labels = examples[num_examples:]["label"]
 
     # nog niet zeker of dit werkt en hoe het werkt
     # tokenized_examples_formatted = tokenized_examples["input_ids", "attention_mask"]
-    print(examples)
     with torch.no_grad():
-        batch_size = 1
+        batch_size = 8
         base_model_logits = []
-        for i in range(0, len(examples["input"]), batch_size):
+        for i in tqdm(range(0, len(examples["input"]), batch_size)):
             batch_inputs = examples["input"][i:i+batch_size]
             logits = tl_model(batch_inputs)[:, -1, :]
             base_model_logits.append(logits)
@@ -89,10 +115,10 @@ def get_all_text_entailment_things(num_examples, device, metric_name, kl_return_
         validation_data=validation_data,
         validation_labels=validation_labels,
         validation_mask=None,
-        # validation_patch_data=validation_patch_data,
+        validation_patch_data=validation_patch_data,
         test_metrics=test_metrics,
         test_data=test_data,
         test_labels=test_labels,
         test_mask=None,
-        # test_patch_data=test_patch_data,
+        test_patch_data=test_patch_data,
     )
