344fa7d33a904728c3bf8a303893f874bf19fb9a
Andres Van Schel <r0806556@esat.kuleuven.be>
2024-12-28 20:12:00 +0100

    changed "COL" to ":" to correctly support colons in nodes patterns in extract_info function of acdc_utils.py

--- acdc/acdc_utils.py
+++ acdc/acdc_utils.py
@@ -220,7 +220,7 @@ def extract_info(string):
     parent_list = None
     if parent_list_str:
         parent_list_items = parent_list_str.split(", ")
-        parent_list = [ast.literal_eval(item if item != "COL" else "None") for item in parent_list_items]
+        parent_list = [ast.literal_eval(item if item != ":" else "None") for item in parent_list_items]
 
     # Extract current node info
     current_match = re.search(current_pattern, string)
@@ -229,7 +229,7 @@ def extract_info(string):
     current_list = None
     if current_list_str:
         current_list_items = current_list_str.split(", ")
-        current_list = [ast.literal_eval(item if item != "COL" else "None") for item in current_list_items]
+        current_list = [ast.literal_eval(item if item != ":" else "None") for item in current_list_items]
 
     return parent_name.replace("hook_resid_mid", "hook_mlp_in"), parent_list, current_name.replace("hook_resid_mid", "hook_mlp_in"), current_list
 
